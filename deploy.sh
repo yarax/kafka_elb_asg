88ea61f8cef69dcc9c267d4d3cb965ebde79687c
Roman Krivtsov <romankrivtsov@Romans-Air.fritz.box>
2019-03-26 20:07:26 +0100

    with elastic ips

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 
 aws cloudformation deploy --profile thinkport --region eu-central-1 --template-file ./cloudformation/vpc_github.yaml --stack-name KafkaDemoVPC2
 
-aws cloudformation deploy --profile thinkport --region eu-central-1 --template-file ./cloudformation/asg.yaml --stack-name KafkaDemoCluster --parameter-overrides UserData=$(cat ./dynamic_ips.sh | base64)
+aws cloudformation deploy --profile thinkport --region eu-central-1 --template-file ./cloudformation/asg.yaml --stack-name KafkaDemoCluster2 --parameter-overrides UserData=$(cat ./dynamic_ips.sh | base64)
 
 aws cloudformation deploy --profile thinkport --region eu-central-1 --template-file ./cloudformation/ec2_zipkin.yaml --stack-name ZipkinInstance --parameter-overrides UserData=$(cat ./zipkin_userdata.sh | base64)
 
-# aws cloudformation deploy --profile thinkport --region eu-central-1 --template-file ./cloudformation/ec2.yaml --stack-name KafkaDemoSingleInstance2 --parameter-overrides UserData=$(cat ./userdata.sh | base64) --query 'Stacks[0].Outputs[?OutputKey==`KafkaInstance`].OutputValue' --output text  
+# aws cloudformation deploy --profile thinkport --region eu-central-1 --template-file ./cloudformation/ec2.yaml --stack-name KafkaDemoSingleInstance2 --parameter-overrides UserData=$(cat ./userdata.sh | base64) --query 'Stacks[0].Outputs[?OutputKey==`KafkaInstance`].OutputValue' --output text
--- dynamic_ips.sh
+++ dynamic_ips.sh
@@ -1,59 +1,74 @@
 #!/usr/bin/env bash
-sudo -s
-yum install -y jq
+
+# Replace with your Autoscaling Group name
+MY_ASG_NAME="KafkaDemoCluster-WebServerGroup"
+TAG="kafka-brokers-demo"
+
+sudo yum install -y jq
+sudo yum install -y wget
+sudo yum install -y java
+cd ~
+wget "http://apache.lauf-forum.at/kafka/2.1.0/kafka_2.11-2.1.0.tgz"
+tar -xvf kafka_2.11-2.1.0.tgz
+cd kafka_2.11-2.1.0
 
 INSTANCE_LAUNCH_ID=$(curl http://169.254.169.254/latest/meta-data/ami-launch-index)
 MY_INST_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
 
 MY_IP=$(aws ec2 --region eu-central-1 describe-instances --instance-ids $MY_INST_ID --query 'Reservations[*].Instances[*].PublicIpAddress' --output text)
 
-instances=$(aws autoscaling --region eu-central-1 describe-auto-scaling-groups --auto-scaling-group-names KafkaDemoCluster-WebServerGroup)
+# instances=$(aws autoscaling --region eu-central-1 describe-auto-scaling-groups --auto-scaling-group-names $MY_ASG_NAME)
 
 ZOOKEEPER_NODES=""
 ZOOKEEPER_CONNECT=""
 COUNTER=1
 SERVER_I=0
+ASSIGNED=0
 
-for inst in $(echo "${instances}" | jq -rc '.AutoScalingGroups[0] | .Instances[]'); do
-    INST_ID=$(echo $inst | jq -r '.InstanceId')
-    IP=$(aws ec2 --region eu-central-1 describe-instances --instance-ids $INST_ID | jq -rc '.Reservations[0].Instances[0].PublicIpAddress')
-    if [ $MY_IP == $IP ]; then
-        IP='0.0.0.0'
+eips=$(aws ec2 describe-addresses --region eu-central-1 --filter Name=tag-value,Values=$TAG)
+for eip in $(echo "${eips}" | jq -rc '.Addresses[]'); do
+    INST_ID=$(echo $eip | jq -r '.InstanceId')
+    ALLOC=$(echo $eip | jq -r '.AllocationId')
+    IP=$(echo $eip | jq -r '.PublicIp')
+    if [[ $INST_ID == 'null' ]] && [[ ASSIGNED == 0 ]]; then
+        ASSIGNED=1
         SERVER_I=$COUNTER
+        IP='0.0.0.0'
+        echo "Assigned $IP"
+        aws ec2 associate-address --region eu-central-1 --allocation-id $ALLOC --instance-id $MY_INST_ID
     fi
     ZOOKEEPER_NODES=$ZOOKEEPER_NODES"server.$COUNTER=$IP:2888:3888"$'\n'
-    if [ $COUNTER == 1 ]
-    then
+    if [ $COUNTER == 1 ]; then
         ZOOKEEPER_CONNECT="$IP:2181"
     else
         ZOOKEEPER_CONNECT="$ZOOKEEPER_CONNECT,$IP:2181"
     fi
     COUNTER=$(($COUNTER+1))
 done
-echo $SERVER_I
 
-echo $ZOOKEEPER_CONNECT
 echo $ZOOKEEPER_NODES
+echo $ZOOKEEPER_CONNECT
 
-rm -rf /tmp/kafka-logs/*
+# Download default configuration templates
+curl "https://raw.githubusercontent.com/ThinkportRepo/kafka_zipkin_demo/master/default_cfg/server.properties.default" > ./config/server.properties.temp
+curl "https://raw.githubusercontent.com/ThinkportRepo/kafka_zipkin_demo/master/default_cfg/zookeeper.properties.default" > ./config/zookeeper.properties.temp
 
-curl "https://raw.githubusercontent.com/ThinkportRepo/kafka_zipkin_demo/master/default_cfg/server.properties.default" > /home/ec2-user/kafka_2.11-2.1.0/config/server.properties.temp
+sed -i "s/#BROKER_ID#/$SERVER_I/g" ./config/server.properties.temp
+sed -i "s/#ZOOKEEPER_CONNECT#/$ZOOKEEPER_CONNECT/g" ./config/server.properties.temp
+sed -i "s/#ATVERTISED_LISTENERS#/PLAINTEXT:\/\/$MY_IP:9092/g" ./config/server.properties.temp
 
-sed -i "s/broker.id=#BROKER_ID#/$SERVER_I/g" /home/ec2-user/kafka_2.11-2.1.0/config/server.properties.temp
-sed -i "s/#ZOOKEEPER_CONNECT#/$ZOOKEEPER_CONNECT/g" /home/ec2-user/kafka_2.11-2.1.0/config/server.properties.temp
-echo $MY_IP
-sed -i "s/#ATVERTISED_LISTENERS#/PLAINTEXT:\/\/$MY_IP:9092/g" /home/ec2-user/kafka_2.11-2.1.0/config/server.properties.temp
-#sed -i "s/zookeeper.connection.timeout.ms=6000/zookeeper.connection.timeout.ms=180000/" /home/ec2-user/kafka_2.11-2.1.0/config/server.properties.temp
-#zookeeper.connection.timeout.ms=6000
+#sed -i "s/zookeeper.connection.timeout.ms=6000/zookeeper.connection.timeout.ms=180000/" ./config/server.properties.temp
 
-cp /home/ec2-user/kafka_2.11-2.1.0/config/zookeeper.properties.default /home/ec2-user/kafka_2.11-2.1.0/config/zookeeper.properties.temp
+echo "$ZOOKEEPER_NODES" >> ./config/zookeeper.properties.temp
 
-echo "$ZOOKEEPER_NODES" >> /home/ec2-user/kafka_2.11-2.1.0/config/zookeeper.properties.temp
+# Update actual config files
+mv -f ./config/server.properties.temp ./config/server.properties
+mv -f ./config/zookeeper.properties.temp ./config/zookeeper.properties
 
-mv -f /home/ec2-user/kafka_2.11-2.1.0/config/server.properties.temp /home/ec2-user/kafka_2.11-2.1.0/config/server.properties
-mv -f /home/ec2-user/kafka_2.11-2.1.0/config/zookeeper.properties.temp /home/ec2-user/kafka_2.11-2.1.0/config/zookeeper.properties
-
-mkdir -p /home/ec2-user/logs
+mkdir -p ~/logs
+mkdir -p /tmp/zookeeper
+# Set the zookeeper id
 echo $(($SERVER_I)) > /tmp/zookeeper/myid
-/home/ec2-user/kafka_2.11-2.1.0/bin/zookeeper-server-start.sh /home/ec2-user/kafka_2.11-2.1.0/config/zookeeper.properties > /home/ec2-user/logs/zookeeper.log 2>&1 &
-/home/ec2-user/kafka_2.11-2.1.0/bin/kafka-server-start.sh /home/ec2-user/kafka_2.11-2.1.0/config/server.properties > /home/ec2-user/logs/kafka.log 2>&1 &
+# Running 
+./bin/zookeeper-server-start.sh ./config/zookeeper.properties > ~/logs/zookeeper.log 2>&1 &
+./bin/kafka-server-start.sh ./config/server.properties > ~/logs/kafka.log 2>&1 &
